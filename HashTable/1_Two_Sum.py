d793323ee09d66104f70f3456a4beeec292a0ba3
reyno <reynokang@gmail.com>
2017-04-19 00:59:57 +0800

    Two Sum

--- HashTable/1_Two_Sum.py
+++ HashTable/1_Two_Sum.py
@@ -3,6 +3,20 @@
 __author__ = 'Reyno'
 
 class Solution(object):
+    # def twoSum(self, nums, target):
+    #     """
+    #     :type nums: List[int]
+    #     :type target: int
+    #     :rtype: List[int]
+
+    #     O(n)方法，只需遍历一次列表，利用hash表存值和对应的index，利用两个值和为target，
+    #     可以同时获取两者的值，在通过值来获取两个所对应的index。
+    #     """
+    #     hash_table = dict()
+    #     for index,num in enumerate(nums):
+    #         if num in hash_table:
+    #             return [hash_table[num],index,]
+    #         hash_table[target - num] = index
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -13,11 +27,12 @@ class Solution(object):
         可以同时获取两者的值，在通过值来获取两个所对应的index。
         """
         hash_table = dict()
+
         for index,num in enumerate(nums):
-            if num in hash_table:
-                return [hash_table[num],index,]
-            hash_table[target - num] = index
-            
+            check = target - num
+            if check in hash_table:
+                return [hash_table[check],index,]
+            hash_table[num] = index
         
 if __name__ == '__main__':
     nums = [3,2,4]
